a84f8771768986913f70ade14aac8273ca993a0c
Jesus Velazquez <jvelaz46@ford.com>
2020-08-20 15:42:29 -0500

    Add database driver and pasword hashing for sign up and login

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
 const express = require('express')
 const path = require('path')
+const redis = require('redis')
+const bcrypt = require('bcryptjs')
 
 const app = express()
+const client = redis.createClient()
+const salt = bcrypt.genSaltSync(10);
 
 // Setup pug
 app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+
 // Routes
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const {username, password} = req.body
+  if(!username || !password) {
+    res.render('error', {
+      message: 'Please set username and password'
+    })
+    return
+  } else {
+    client.hget('users', username, (err, userid) => {
+      if (!userid) {
+        //user does not exist, signup procedure
+        console.log('sign up user')
+        client.incr('userid', async (err, userid) => {
+          client.hset('users', username, userid)
+          const hash = bcrypt.hashSync(password, salt)
+          client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+        })
+      } else {
+        //user exists, login procedure
+        console.log('sign in user')
+        client.hget(`user:${userid}`, 'hash', async (err, hash) => {
+          if (err) console.log(err)
+          const result = await bcrypt.compare(password, hash)
+          if (result) {
+            //password ok
+          } else {
+            //wrong password
+          }
+        })
+      }
+    })
+  }
+  res.end()
 })
 
 // Listen
